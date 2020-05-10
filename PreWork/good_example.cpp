5a0fb33c77ca7cb50097c0b0cc8a2be54769cb58
Łukasz Ziobroń <lukasz@coders.school>
2020-05-10 10:18:26 +0200

    Update good_example.cpp

--- PreWork/good_example.cpp
+++ PreWork/good_example.cpp
@@ -1,34 +1,39 @@
 bool Game::CheckWinCondition() const {
-	return player_->GetMoney() >= final_goal_;
+    return player_->GetMoney() >= final_goal_;
 }
 
 bool Game::CheckLooseCodition() const {
-	return player_->GetMoney() == SIZE_MAX;
+    return player_->GetMoney() == SIZE_MAX;
 }
 
 void Game::PrintMenu() {
-	std::cout << "Money: " << player_->GetMoney() 
-		<< " Day: " << time_->GetElpasedTime()
-		<< " Days left: " << days_ - time_->GetElpasedTime()
-		<< " Current position: " 
-		<< map_->GetCurrentPosition()->GetCoordinates() << "\n";
+    std::cout << "Money: " << player_->GetMoney() 
+              << " Day: " << time_->GetElpasedTime()
+              << " Days left: " << days_ - time_->GetElpasedTime()
+              << " Current position: " 
+              << map_->GetCurrentPosition()->GetCoordinates() << "\n";
 }
 
 void Game::PrintOptions() {
-	std::cout << "\n" << "1) Travel\n" << "2) Sell\n" << "3) Buy\n"
-		<< "4) Check Cargo\n" << "0) Exit\n\n" << "Choice: ";
+    std::cout << "\n"
+              << "1) Travel\n"
+              << "2) Sell\n"
+              << "3) Buy\n"
+              << "4) Check Cargo\n"
+              << "0) Exit\n\n"
+              << "Choice: ";
 }
 
 void Game::PrintWinScreen() {
-	system("cls");
-	std::cout << "CONGRATULATIUON! You earn: " << player_->GetMoney()
-		<< " money in: " << time_->GetElpasedTime() << " days";
-	std::this_thread::sleep_for(5s);
+    system("cls");
+    std::cout << "CONGRATULATIUON! You earn: " << player_->GetMoney()
+              << " money in: " << time_->GetElpasedTime() << " days";
+    std::this_thread::sleep_for(5s);
 }
 
 void Game::PrintLooseScreen() {
-	system("cls");
-	std::cout << "GAME OVER! You earn: " << player_->GetMoney()
-		<< " money in: " << time_->GetElpasedTime() << " days";
-	std::this_thread::sleep_for(5s);
+    system("cls");
+    std::cout << "GAME OVER! You earn: " << player_->GetMoney()
+              << " money in: " << time_->GetElpasedTime() << " days";
+    std::this_thread::sleep_for(5s);
 }
