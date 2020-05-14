64e57bae702528a0fc5b607d0151e1690e40157e
Patryk Łapiezo <piezolpiezol@gmail.com>
2020-05-14 17:13:51 +0000

    Drugie zadanie (max)

--- module1/task2.cpp
+++ module1/task2.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 // Write your function here
+int max (int a, int b){
+  return a > b ? a : b;
+}
+
+int max (int a, int b, int c){
+  return max(max(a,b),c);
+}
+
 
 int main() {
     std::cout << "max (1, 2, 3): " << max(1, 2, 3) << "\n";
