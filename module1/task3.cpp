731532a6d40bc465121749177d98744c48b7b43f
Patryk Łapiezo <piezolpiezol@gmail.com>
2020-05-14 18:22:03 +0000

    Third task

--- module1/task3.cpp
+++ module1/task3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 // Write your function here
+void printString(const char * str, const int times) {
+  for(int i = 0; i < times; i++){
+    std::cout << str;
+  }
+}
 
 int main() {
     printString("Hello", 5);
