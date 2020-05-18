ce3066ebaeefc468bb59ea154484d25e19052093
piezol <piezolpiezol@gmail.com>
2020-05-18 22:25:56 +0200

    Iterative fibo

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) return 0;
+    int fib_n_1 = 0, fib_n = 1;
+    int temporary_sum;
+    while(--sequence > 0 ){
+        temporary_sum = fib_n_1 + fib_n;
+        fib_n_1 = fib_n;
+        fib_n = temporary_sum;
+    }
+    return fib_n;
 }
 
 int fibonacci_recursive(int sequence) {
