58dd7688452e0ae3ae518cbbfdd1892d0d461179
piezol <piezolpiezol@gmail.com>
2020-05-22 17:29:05 +0200

    All tests pass - generateSequence

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -1,7 +1,15 @@
 #pragma once
 #include <vector>
-
+#include<iostream>
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+    if (count <= 0){
+        return vec;
+    }
+    vec.reserve(count);
+    int currentnum = 0;
+    while(count--){
+        vec.push_back(currentnum += step);
+    }
+    return vec;
 }
