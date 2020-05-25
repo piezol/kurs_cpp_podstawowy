8015ee6b0d13a6773c396d792e64959cf453246a
mefox256 <mefox256@gmail.com>
2020-05-25 21:34:24 +0200

    revert changes

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,13 +2,6 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    std::vector<int> result;
-    if(count == 0 || step == 0){
-        return {0};
-    }
-    for(int i=0, j = step; i<count; i++, j+=step){
-        result.push_back(j);
-    }
-
-    return result;
+    // TODO: Implement me :)
+    return {};
 }
