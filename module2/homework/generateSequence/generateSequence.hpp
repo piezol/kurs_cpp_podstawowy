fa83c477e958ccb17582fedfbf19cc52bad74acd
mefox256 <mefox256@gmail.com>
2020-05-25 17:05:30 +0200

    ready

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,13 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> result;
+    if(count == 0 || step == 0){
+        return {0};
+    }
+    for(int i=0, j = step; i<count; i++, j+=step){
+        result.push_back(j);
+    }
+
+    return result;
 }
--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,29 @@
 #pragma once
-
+#include <math.h>
+#include <algorithm>
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs==0 || rhs==0){
+        return std::max(lhs, rhs);
+    }
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    int temp;
+    while(rhs != 0){
+        temp = rhs;
+        rhs = lhs % rhs;
+        lhs = temp;
+    }
+
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if(lhs == 0 || rhs == 0){
+        return 0;
+    }
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+    int result;
+    result = lhs/NWD(lhs, rhs) * rhs;
+    return result;
 }
