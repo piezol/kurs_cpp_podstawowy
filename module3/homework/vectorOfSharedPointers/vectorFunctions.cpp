9beb79264a687b3a144def33d6163a1ddb5d35c3
piezol <piezolpiezol@gmail.com>
2020-05-29 20:50:18 +0200

    All 5 functions pass tests

---
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count)
+{
+    if(count < 1)
+        return {};
+
+    std::vector<std::shared_ptr<int>> vec(count);
+
+    for(int i = 0; i < count; i++){
+        vec[i] = std::make_shared<int>(i);    
+    }
+
+    return vec;
+}
+
+void print(const std::vector<std::shared_ptr<int>>& vec)
+{
+    for(const auto& el : vec) {
+        std::cout << *el << '\t'; 
+    }
+}
+
+void add10(const std::vector<std::shared_ptr<int>>& vec)
+{
+   for (auto& el : vec) {
+       if(el){
+            *el += 10;
+       }
+   }
+
+}
+void sub10(int * ptr)
+{
+    if(ptr != nullptr){
+        *ptr -= 10;
+    }
+}
+void sub10(const std::vector<std::shared_ptr<int>>& vec)
+{
+    for (auto& el : vec) {
+        sub10(el.get());
+    }
+}
