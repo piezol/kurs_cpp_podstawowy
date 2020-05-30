0f7a10a19b81192f564caf4028eed3943cdf59e2
piezol <piezolpiezol@gmail.com>
2020-05-30 15:28:31 +0200

    Enum added

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,8 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+}
