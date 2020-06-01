58d70c1cdd99dfdbcb5ee264a33a025585589c3b
piezol <piezolpiezol@gmail.com>
2020-06-01 23:35:11 +0200

    Reformat WebKit

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -4,71 +4,73 @@
 #include <cctype>
 #include <utility>
 
-
-
-std::string getErrorMessage(const ErrorCode errorCode) {
-    switch(errorCode)
-    {
-        case ErrorCode::Ok:
-            return "OK";
-        case ErrorCode::PasswordNeedsAtLeastNineCharacters:
-            return "Password needs at least nine characters";
-        case ErrorCode::PasswordNeedsAtLeastOneNumber:
-            return "Password needs at least one number";
-        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
-            return "Password needs at least one special character";
-        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
-            return "Password needs at least one uppercase letter";
-        case ErrorCode::PasswordsDoesNotMatch:
-            return "Passwords don't match";
-        default:
-            return "Unrecognized error code";
+std::string getErrorMessage(const ErrorCode errorCode)
+{
+    switch (errorCode) {
+    case ErrorCode::Ok:
+        return "OK";
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+        return "Password needs at least nine characters";
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
+        return "Password needs at least one number";
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+        return "Password needs at least one special character";
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+        return "Password needs at least one uppercase letter";
+    case ErrorCode::PasswordsDoesNotMatch:
+        return "Passwords don't match";
+    default:
+        return "Unrecognized error code";
     }
-
 }
-
 
 typedef std::pair<char, char> charRange;
 
-bool strHasDecimalDigits(const std::string& str) {
+bool strHasDecimalDigits(const std::string& str)
+{
     return std::any_of(str.begin(), str.end(), ::isdigit);
 }
 
-bool strHasUppercaseLetters(const std::string& str) {
+bool strHasUppercaseLetters(const std::string& str)
+{
     return std::any_of(str.begin(), str.end(), ::isupper);
 }
 
-bool strHasSpecialCharacters(const std::string& str) {
+bool strHasSpecialCharacters(const std::string& str)
+{
     return std::any_of(str.begin(), str.end(), std::not1(std::ptr_fun(::isalnum)));
 }
 
-bool doesPasswordsMatch(const std::string& firstPassword, const std::string& secondPassword) {
+bool doesPasswordsMatch(const std::string& firstPassword, const std::string& secondPassword)
+{
     return firstPassword == secondPassword;
 }
 
-ErrorCode checkPasswordRules(const std::string& password) {
+ErrorCode checkPasswordRules(const std::string& password)
+{
 
-    if(password.length() < MIN_PASSWORD_LENGTH){
+    if (password.length() < MIN_PASSWORD_LENGTH) {
         return ErrorCode::PasswordNeedsAtLeastNineCharacters;
     }
 
-    if(!strHasDecimalDigits(password)){
+    if (!strHasDecimalDigits(password)) {
         return ErrorCode::PasswordNeedsAtLeastOneNumber;
     }
 
-    if(!strHasUppercaseLetters(password)) {
+    if (!strHasUppercaseLetters(password)) {
         return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
     }
 
-    if(!strHasSpecialCharacters(password)) {
+    if (!strHasSpecialCharacters(password)) {
         return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
     }
 
     return ErrorCode::Ok;
 }
 
-ErrorCode checkPassword(const std::string& firstPassword, const std::string& secondPassword){
-    if(!doesPasswordsMatch(firstPassword, secondPassword)){
+ErrorCode checkPassword(const std::string& firstPassword, const std::string& secondPassword)
+{
+    if (!doesPasswordsMatch(firstPassword, secondPassword)) {
         return ErrorCode::PasswordsDoesNotMatch;
     }
 
