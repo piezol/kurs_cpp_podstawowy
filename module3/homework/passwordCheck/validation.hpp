6c6005f9b070165da225349894c195bebb2eb133
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-03 20:38:38 +0200

    Implemented header file with enum class and functions declarations

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,18 @@
 // TODO: I'm empty :) Put enum and function headers here.
 // Don't forget the header guard
+#pragma once
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode errorCode);
+bool doesPasswordsMatch(std::string firstPassword, std::string secondPassword);
+ErrorCode checkPasswordRules(std::string password);
+ErrorCode checkPassword(std::string firstPassword, std::string secondPassword);
