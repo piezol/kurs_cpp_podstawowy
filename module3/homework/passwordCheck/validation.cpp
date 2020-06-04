30fae5df03d30e402d5b0a7cda32b99a0e45a1f9
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-04 22:02:34 +0200

    implemented all required functions in cpp. startedadditional tasks

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,45 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode errorCode) {
+    std::string result{};
+    switch (errorCode) {
+    case (ErrorCode::Ok):
+        result = "OK";
+        break;
+    case (ErrorCode::PasswordNeedsAtLeastNineCharacters):
+        result = "Password Needs At Least Nine Characters";
+        break;
+    case (ErrorCode::PasswordNeedsAtLeastOneNumber):
+        result = "Password Needs At Least One Number";
+        break;
+    case (ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter):
+        result = "Password Needs At Least One Special Character";
+        break;
+    case (ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter):
+        result = "Password Needs At Least One Uppercase Letter";
+        break;
+    case (ErrorCode::PasswordsDoesNotMatch):
+        result = "Passwords Does Not Match";
+        break;
+    default:
+        result = "Unknow Error Code";
+        break;
+    }
+    return result;
+}
+bool doesPasswordsMatch(std::string firstPassword, std::string secondPassword) {
+    return (firstPassword == secondPassword);  
+}
+
+ErrorCode checkPasswordRules(std::string password) {
+
+    return ErrorCode::Ok;
+}
+
+ErrorCode checkPassword(std::string firstPassword, std::string secondPassword) {
+    if(!doesPasswordsMatch(firstPassword, secondPassword)) {
+        return ErrorCode::PasswordsDoesNotMatch;
+    }
+    // tu wywolac checkPasswordRules
+    return ErrorCode::Ok;
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,5 +1,3 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
 #pragma once
 #include <string>
 
