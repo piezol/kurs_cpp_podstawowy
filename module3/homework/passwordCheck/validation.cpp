9b6095404c5013697e0d3ca800c2d27051d321d5
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-04 23:07:31 +0200

    added additional checks in checkPasswordRules function

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,24 +1,26 @@
 #include "validation.hpp"
 
+#include <algorithm>
+
 std::string getErrorMessage(ErrorCode errorCode) {
     std::string result{};
     switch (errorCode) {
-    case (ErrorCode::Ok):
+    case ErrorCode::Ok:
         result = "OK";
         break;
-    case (ErrorCode::PasswordNeedsAtLeastNineCharacters):
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         result = "Password Needs At Least Nine Characters";
         break;
-    case (ErrorCode::PasswordNeedsAtLeastOneNumber):
+    case ErrorCode::PasswordNeedsAtLeastOneNumber:
         result = "Password Needs At Least One Number";
         break;
-    case (ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter):
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         result = "Password Needs At Least One Special Character";
         break;
-    case (ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter):
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         result = "Password Needs At Least One Uppercase Letter";
         break;
-    case (ErrorCode::PasswordsDoesNotMatch):
+    case ErrorCode::PasswordsDoesNotMatch:
         result = "Passwords Does Not Match";
         break;
     default:
@@ -27,19 +29,30 @@ std::string getErrorMessage(ErrorCode errorCode) {
     }
     return result;
 }
-bool doesPasswordsMatch(std::string firstPassword, std::string secondPassword) {
-    return (firstPassword == secondPassword);  
+
+bool doesPasswordsMatch(const std::string& firstPassword, const std::string& secondPassword) {
+    return (firstPassword == secondPassword);
 }
 
-ErrorCode checkPasswordRules(std::string password) {
-
+ErrorCode checkPasswordRules(const std::string& password) {
+    if (password.length() < MIN_LENGTH) {
+        return ErrorCode::PasswordNeedsAtLeastNineCharacters;
+    }
+    if (std::none_of(password.begin(), password.end(), [](unsigned char c) { return std::isdigit(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneNumber;
+    }
+    if (std::none_of(password.begin(), password.end(), [](unsigned char c) { return std::ispunct(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+    }
+    if (std::none_of(password.begin(), password.end(), [](unsigned char c) { return std::isupper(c); })) {
+        return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
+    }
     return ErrorCode::Ok;
 }
 
-ErrorCode checkPassword(std::string firstPassword, std::string secondPassword) {
-    if(!doesPasswordsMatch(firstPassword, secondPassword)) {
+ErrorCode checkPassword(const std::string& firstPassword, const std::string& secondPassword) {
+    if (!doesPasswordsMatch(firstPassword, secondPassword)) {
         return ErrorCode::PasswordsDoesNotMatch;
     }
-    // tu wywolac checkPasswordRules
-    return ErrorCode::Ok;
+    return checkPasswordRules(firstPassword);
 }
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,5 +1,8 @@
 #pragma once
+
 #include <string>
+
+constexpr unsigned int MIN_LENGTH = 9;
 
 enum class ErrorCode {
     Ok,
@@ -11,6 +14,6 @@ enum class ErrorCode {
 };
 
 std::string getErrorMessage(ErrorCode errorCode);
-bool doesPasswordsMatch(std::string firstPassword, std::string secondPassword);
-ErrorCode checkPasswordRules(std::string password);
-ErrorCode checkPassword(std::string firstPassword, std::string secondPassword);
+bool doesPasswordsMatch(const std::string& firstPassword, const std::string& secondPassword);
+ErrorCode checkPasswordRules(const std::string& password);
+ErrorCode checkPassword(const std::string& firstPassword, const std::string& secondPassword);
